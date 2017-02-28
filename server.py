0d4368ca4071dd5dc06b22de3585d2302c410ad7
aribis369 <aribis369@gmail.com>
2017-02-28 20:22:49 +0530

    sever in python

---
+++ server.py
@@ -0,0 +1,38 @@
+import socket
+
+
+sdata=[]
+adata=''
+print "prog starts"
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind(("127.0.0.1",8000))
+sock.listen(2)
+(client,(ip,port))=sock.accept()
+def rec():
+    global sdata
+    global adata
+    rdata=client.recv(2012)
+    print rdata
+    sdata=rdata.split(',')
+    sdata.sort()
+    for i in sdata:
+        adata=adata+i+','
+    client.send(adata)
+    rec()
+   
+
+
+rec()
+
+
+
+
+
+
+
+
+
+
+
+
+
