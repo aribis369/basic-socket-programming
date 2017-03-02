c958a0c8984c3e1888675dc6a9fa784dc94732fd
aribis369 <aribis369@gmail.com>
2017-03-03 01:52:31 +0800

    Update server.py

--- server.py
+++ server.py
@@ -7,16 +7,17 @@ print "prog starts"
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.bind(("127.0.0.1",8000))
 sock.listen(2)
+(client,(ip,port))=sock.accept()
 def rec():
     global sdata
     global adata
-    (client,(ip,port))=sock.accept()
     rdata=client.recv(2012)
     print rdata
     sdata=rdata.split(',')
     sdata.sort()
     for i in sdata:
         adata=adata+i+','
+    adata=adata[:-1]
     client.send(adata)
     adata=''
     rec()
